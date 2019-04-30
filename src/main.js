8d2defc590bea498464b82e71bd5e7dc9f31ca3b
Ryan Quellhorst <rquellhorst@hmbnet.com>
2019-04-30 12:19:20 -0400

    added Vuetify

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 export default function (Vue, { router, head, isClient }) {
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+  })
+
+  //Setup Vuetify
+  Vue.use(Vuetify)
+  
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
