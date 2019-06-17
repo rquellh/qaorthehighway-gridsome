8ea0fae3abeec486c25c2d574d52eb7b60290817
Ryan Quellhorst <rquellhorst@aclens.local>
2019-06-17 12:54:57 -0400

    added sponsors section and refactored the home page

---
+++ src/layouts/HomePageLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="layout">
+    <Navbar/>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
+
+
+<style>
+body {
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+  font-size: 16px;
+}
+
+.layout {
+  max-width: 100%;
+  margin: 0 auto;
+  padding-left: 0px;
+  padding-right: 0px;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,71 +1,97 @@
 <template>
   <v-app>
-    <Navbar/>
-    <v-content>
-      <Hero/>
-      <div class="mission">
-        <v-container py-5 fluid>
-          <v-layout class="max-width" row wrap align-center>
-            <v-flex xs12 md4 class="text-xs-center" v-for="mission in missions" :key="mission.index">
-              <v-img 
-              :src="mission.img"
-              :aspect-ratio="10/3"
-              contain> 
-              </v-img>
-              <h1 class="pt-5">{{mission.title}}</h1>
-              <p class="px-5 pb-3">{{mission.description}}</p>
-            </v-flex>
-          </v-layout>
-          
-          
-          <!-- :class="['text-xs-center', {'display-1 pa-5 font-weight-bold' : $vuetify.breakpoint.mdAndUp}]" -->
-        </v-container>
-      </div>
-      <div class="image-break">
-        <v-container fluid ma-0 pa-0>
-          <v-layout class="max-width" ma-0 pa-0>
-            <v-flex xs12 md6 >
-              <v-img
-                :aspect-ratio="16/6"
-                :src="require('@/assets/images/transition/transition-1.png')"
-              ></v-img>
-            </v-flex>
-            <v-flex md6 class="hidden-sm-and-down">
-              <v-img
-                :aspect-ratio="16/6"
-                :src="require('@/assets/images/transition/transition-2.png')"
-              ></v-img>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </div>
-      <div class="sponsors">
-        <v-container fluid ma-0 pa-0>
-          <v-layout class="max-width" ma-0 pa-0 row>
-            <v-flex
-              xs6
-              ma-3
-              pa-3
-              align-self-center
-              v-for="platinum in platinums"
-              :key="platinum.index"
-            >
-              <v-img class="center" :src="platinum.img" max-height="500" max-width="500"></v-img>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </div>
-    </v-content>
+    <HomePageLayout>
+      <v-content>
+        <Hero/>
+        <div class="mission">
+          <v-container py-5 fluid>
+            <v-layout row wrap align-center>
+              <v-flex
+                xs12
+                md4
+                class="text-xs-center"
+                v-for="mission in missions"
+                :key="mission.index"
+              >
+                <v-img :src="mission.img" :aspect-ratio="10/3" contain></v-img>
+                <h1 class="pt-5">{{mission.title}}</h1>
+                <p class="px-5 pb-3">{{mission.description}}</p>
+              </v-flex>
+            </v-layout>
+
+            <!-- :class="['text-xs-center', {'display-1 pa-5 font-weight-bold' : $vuetify.breakpoint.mdAndUp}]" -->
+          </v-container>
+        </div>
+        <div class="image-break">
+          <v-container fluid ma-0 pa-0>
+            <v-layout ma-0 pa-0>
+              <v-flex xs12 md6>
+                <v-img
+                  :aspect-ratio="16/6"
+                  :src="require('@/assets/images/transition/transition-1.png')"
+                ></v-img>
+              </v-flex>
+              <v-flex md6 class="hidden-sm-and-down">
+                <v-img
+                  :aspect-ratio="16/6"
+                  :src="require('@/assets/images/transition/transition-2.png')"
+                ></v-img>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </div>
+        <div class="sponsors">
+          <v-container fluid>
+            <h1 class="my-5 text-xs-center primary--text underline-primary">Platinum Sponsors</h1>
+            <v-layout align-center row fill-height wrap>
+              <v-flex xs12 md6 pa-5 v-for="platinum in platinums" :key="platinum.index">
+                <v-img :src="platinum.img" :aspect-ratio="1.9" contain></v-img>
+              </v-flex>
+            </v-layout>
+            <h1 class="my-5 text-xs-center secondary--text underline-secondary">Gold Sponsors</h1>
+            <v-layout align-center row fill-height wrap>
+              <v-flex pa-5 xs6 md4 align-self-center v-for="gold in 5" :key="gold.index">
+                <v-img
+                  :src="require('@/assets/images/generic-profile.png')"
+                  :aspect-ratio="1.25"
+                  contain
+                ></v-img>
+              </v-flex>
+            </v-layout>
+            <h1 class="my-5 text-xs-center primary--text underline-primary">Silver Sponsors</h1>
+            <v-layout align-center row fill-height wrap>
+              <v-flex sm4 md3 xs6 pa-5 align-self-center v-for="silver in 5" :key="silver.index">
+                <v-img
+                  :src="require('@/assets/images/generic-profile.png')"
+                  :aspect-ratio="1.25"
+                  contain
+                ></v-img>
+              </v-flex>
+            </v-layout>
+            <h1 class="my-5 text-xs-center secondary--text underline-secondary">Lunch & Snack Sponsors</h1>
+            <v-layout align-center row fill-height wrap>
+              <v-flex xs6 pa-5 align-self-center v-for="silver in 2" :key="silver.index">
+                <v-img
+                  :src="require('@/assets/images/generic-profile.png')"
+                  :aspect-ratio="1.25"
+                  contain
+                ></v-img>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </div>
+      </v-content>
+    </HomePageLayout>
   </v-app>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import HomePageLayout from "@/layouts/HomePageLayout";
 
 export default {
   components: {
-    Navbar,
+    HomePageLayout,
     Hero
   },
   data() {
@@ -73,18 +99,21 @@ export default {
       missions: [
         {
           title: "One Day",
-          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum numquam aperiam, dolorem officia dignissimos quidem natus quo nobis, repellendus qui odio consequatur voluptatum similique excepturi? Labore deserunt ipsa totam nam.",
-          img: require('@/assets/images/mission/day-one.png')
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum numquam aperiam, dolorem officia dignissimos quidem natus quo nobis, repellendus qui odio consequatur voluptatum similique excepturi? Labore deserunt ipsa totam nam.",
+          img: require("@/assets/images/mission/day-one.png")
         },
         {
           title: "Affordable",
-          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum numquam aperiam, dolorem officia dignissimos quidem natus quo nobis, repellendus qui odio consequatur voluptatum similique excepturi? Labore deserunt ipsa totam nam.",
-          img: require('@/assets/images/mission/piggy-bank.png')
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum numquam aperiam, dolorem officia dignissimos quidem natus quo nobis, repellendus qui odio consequatur voluptatum similique excepturi? Labore deserunt ipsa totam nam.",
+          img: require("@/assets/images/mission/piggy-bank.png")
         },
         {
           title: "Thought Leadership",
-          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum numquam aperiam, dolorem officia dignissimos quidem natus quo nobis, repellendus qui odio consequatur voluptatum similique excepturi? Labore deserunt ipsa totam nam.",
-          img: require('@/assets/images/mission/idea.png')
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum numquam aperiam, dolorem officia dignissimos quidem natus quo nobis, repellendus qui odio consequatur voluptatum similique excepturi? Labore deserunt ipsa totam nam.",
+          img: require("@/assets/images/mission/idea.png")
         }
       ],
       platinums: [
@@ -102,15 +131,14 @@ export default {
   metaInfo: {
     title: ""
   }
-  
 };
 </script>
 
 <style scoped>
-.max-width {
-  max-width: 100%;
+.underline-primary {
+  border-bottom: 1px solid var(--v-primary-base);
 }
-.center {
-  margin: auto;
+.underline-secondary {
+  border-bottom: 1px solid var(--v-secondary-base);
 }
 </style>
