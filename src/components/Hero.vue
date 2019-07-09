79c08bb6e4450af1b9eac67a7b47e8fa23ea1a69
Ryan Quellhorst <rquellhorst@hmbnet.com>
2019-07-09 10:26:04 -0400

    removed the background from homepage

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,44 +1,47 @@
 <template>
   <v-container fluid class="gradient" ma-0 pa-0>
-    <div class="geometric-background">
-      <v-layout class="max-width" row pa-0 align-center fill-height>
-        <v-flex sm12 md8 ma-4>
-          <v-img
-            class="center"
-            max-width="950"
-            min-width="280"
-            :src="require('@/assets/images/qaorthehighwaylogo.png')"
+    <v-layout class="max-width" row pa-0 align-center fill-height>
+      <v-flex sm12 md8 ma-4>
+        <v-img
+          class="center"
+          max-width="950"
+          min-width="280"
+          :src="require('@/assets/images/qaorthehighwaylogo.png')"
+        >
+          <div class="fill-height"/>
+        </v-img>
+        <div
+          :class="['text-xs-center white--text font-weight-bold', {'display-2 pt-5' : $vuetify.breakpoint.mdAndUp}, {'headline pt-3' : $vuetify.breakpoint.xsOnly}, {'display-1 pt-4' : $vuetify.breakpoint.smOnly}]"
+        >February 19, 2019</div>
+        <div
+          :class="['text-xs-center outline', {'pt-2' : $vuetify.breakpoint.xsOnly}, {'pt-5' : $vuetify.breakpoint.smAndUp}]"
+        >
+          <v-btn
+            outline
+            dark
+            :class="['pa-4 font-weight-bold', {'title' : $vuetify.breakpoint.xsOnly}, {'headline' : $vuetify.breakpoint.smAndUp}]"
+            href="/outline"
           >
-            <div class="fill-height"/>
-          </v-img>
-          <div :class="['text-xs-center white--text font-weight-bold', {'display-2 pt-5' : $vuetify.breakpoint.mdAndUp}, {'headline pt-3' : $vuetify.breakpoint.xsOnly}, {'display-1 pt-4' : $vuetify.breakpoint.smOnly}]">February 19, 2019</div>
-          <div :class="['text-xs-center outline', {'pt-2' : $vuetify.breakpoint.xsOnly}, {'pt-5' : $vuetify.breakpoint.smAndUp}]">
-            <v-btn outline dark :class="['pa-4 font-weight-bold', {'title' : $vuetify.breakpoint.xsOnly}, {'headline' : $vuetify.breakpoint.smAndUp}]" href="/outline">
-              <v-icon class="pr-4">fa-ticket-alt</v-icon>Get Tickets
-            </v-btn>
-          </div>
-        </v-flex>
-        <v-flex md4 class="hidden-sm-and-down">
-          <v-img :aspect-ratio="9/14" :src="require('@/assets/images/hero_image3.png')">
-            <div class="fill-height"/>
-          </v-img>
-        </v-flex>
-      </v-layout>
-    </div>
+            <v-icon class="pr-4">fa-ticket-alt</v-icon>Get Tickets
+          </v-btn>
+        </div>
+      </v-flex>
+      <v-flex md4 class="hidden-sm-and-down">
+        <v-img :aspect-ratio="9/14" :src="require('@/assets/images/hero_image3.png')">
+          <div class="fill-height"/>
+        </v-img>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <style scoped>
-.geometric-background {
-  background: url("../assets/images/trianglify.png");
-  background-size: cover;
-}
 
 .gradient {
   display: block;
   position: relative;
-  background: linear-gradient(330deg, #626aaf, #000428);
-  background-size: 150% 150%;
+  background: linear-gradient(330deg, var(--v-primary-darken1), #000000);
+  background-size: 200% 200%;
 
   -webkit-animation: gradient-animator 10s ease infinite;
   -moz-animation: gradient-animator 10s ease infinite;
