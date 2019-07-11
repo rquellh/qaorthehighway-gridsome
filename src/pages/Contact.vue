d7b7ead7edb18269c1bc4276a98328c1892494f5
Ryan Quellhorst <rquellhorst@hmbnet.com>
2019-07-11 09:50:06 -0400

    finished the contact page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -4,18 +4,40 @@
       <v-content class="my-5">
         <h1 class="grey--text">Contact</h1>
         <v-divider />
-        <h3
-          class="my-3"
-        >​We'd love to hear from you about new ideas, feedback, questions, or anything else that comes to mind. Pick your favorite medium to reach out to us.</h3>
-        <v-container>
-            <v-layout row>
-                <v-flex xs6>
-                    <div>Email</div>
-                </v-flex>
-                <v-flex xs6>
-                    <div>Social</div>
-                </v-flex>
-            </v-layout>
+        <div
+          class="my-3 subheading"
+        >​We'd love to hear from you about new ideas, feedback, questions, or anything else that comes to mind. Pick your favorite medium to reach out to us.</div>
+        <v-container fluid pa-0 ma-0>
+          <v-layout row pa-0 ma-0>
+            <v-flex xs6 ma-3>
+              <div class="headline secondary--text">
+                <v-icon class="pr-3" color="secondary">fas fa-envelope</v-icon>Email
+                <v-divider />
+              </div>
+              <div class="mt-3" v-for="email in emails" :key="email.type">
+                <div class="subheading">
+                  <v-icon class="pr-3" color="secondary">{{email.icon}}</v-icon>
+                  {{email.type}}:
+                  <a :href="'mailto:' + email.address">{{email.address}}</a>
+                </div>
+              </div>
+            </v-flex>
+            <v-flex xs6 ma-3>
+              <div class="headline primary--text">
+                <v-icon class="pr-3" color="primary">fas fa-users</v-icon>Social
+              </div>
+              <v-divider />
+              <div class="mt-1" v-for="social in socials" :key="social.type">
+                <div class="subheading">
+                  
+                  <v-btn icon target="_blank" :url="social.url">
+                    <v-icon color="primary">{{ social.icon }}</v-icon>
+                  </v-btn> 
+                  <a :href="social.url">{{social.type}}</a>
+                </div>
+              </div>
+            </v-flex>
+          </v-layout>
         </v-container>
       </v-content>
     </Layout>
@@ -26,6 +48,34 @@
 export default {
   metaInfo: {
     title: "Contact"
+  },
+  data() {
+    return {
+      emails: [
+        {
+          type: "Sponsors",
+          icon: "far fa-handshake",
+          address: "qaorthehighway@gmail.com"
+        },
+        {
+          type: "General",
+          icon: "far fa-question-circle",
+          address: "qaorthehighway@gmail.com"
+        }
+      ],
+      socials: [
+        {
+          type: "Twitter",
+          icon: "fab fa-twitter",
+          url: "https://twitter.com/QAortheHighway"
+        },
+        {
+          type: "LinkedIn",
+          icon: "fab fa-linkedin",
+          url: "https://www.linkedin.com/company/central-ohio-software-quality-assurance-and-management/"
+        }
+      ]
+    };
   }
 };
 </script>
